3ed2e7fdb26c4ed83cc100d945adc4074b39cb72
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-09-04 21:28:13 +0200

    laouts created

---
+++ resources/views/resources/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> <title>Farm to market</title></head>
+<body>
+
+    @yield('main')
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MarketController@index');
+Route::resource('markets', 'MarketController');
+
